d4a50d48c988eb441d853f1c323eca7c52369e1e
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-11-09 17:30:40 +0700

    More simple frontend code with two Vue instance.

--- src.js
+++ src.js
@@ -4,69 +4,96 @@ window.brol = new Vue({
   el : '#brol',
   data : {
     currentRoom : 'main',
-    credential : {},
+    credential : {
+    },
     socket : io(),
     messageInput : '',
-    messages : {
-      main : [],
-    }
+    rooms : {
+      main : {
+        name : 'main',
+        messages : [],
+      },
+    },
+    online : [],
+    authenticated : false,
   },
   methods : {
-    askCredential : function(){
-      this.credential.username = prompt('Username : ');
-      this.credential.password = prompt('password : ');
-      console.log(this.credential);
-      this.socket = io();
-      this.socket.emit('authentication', {username : this.credential.username, password : this.credential.password});
-    },
+    // Main ethods
     switchRoom : function(room) {
-      if (room) {
-        this.currentRoom = room;
-      } else {
-        if (this.currentRoom === 'main') {
-        } else {
-        }
-      } 
+      this.currentRoom = room;
+      if (!this.rooms[room]) {
+        this.$set(this.rooms, room, {
+          name : room,
+          messages : [],
+        })
+      }
     },
     sendMessage : function() {
+      if (!this.messageInput || (this.messageInput && this.messageInput.length < 1)) {
+        return;
+      }
 	    var msg = { username : this.credential.username , message : this.messageInput }
 	    if (this.currentRoom !== 'main') {
 	    	msg.recipient = this.currentRoom;
 	    }
-      this.socket.emit('message', msg);
+      console.log(msg);
+      window.socket.emit('message', msg);
       this.messageInput = '';
     },
     privateChat : function(username) {
-    	if (this.credential.username == username) {
-    		return;	
-    	}
-    	this.switchRoom(username);
     }
   }
 })
-
-window.brol.askCredential();
-
-// Events
-window.brol.socket.on('unauthorized', function(err) {
-  alert(err.message);
-  window.brol.askCredential();
-});
-
-window.brol.socket.on('message', function(msg) {
-  console.log(msg);
-	if (msg.recipient) {
-		if (window.brol.credential.username === msg.recipient) {
-			window.brol.privateChat(msg.username);
-		} else {
-			window.brol.privateChat(msg.recipient);
-		}
-    return $('#privatemessages').append($('<li>').text(msg.username + ' : ' + msg.message));
-	}
-  window.brol.messages.main.push(msg);
-});
-
-window.brol.socket.on('onlineUsers', function(online) {
-  window.brol.online = online;
+window.auth = new Vue({
+  el  : '#auth',
+  data : {
+    credential : {
+    },
+    authenticated : false,
+  },
+  methods : {
+    logout : function(){
+      this.credential = {};
+      this.authenticated = false;
+      window.brol.credential = {};
+      window.brol.authenticated = false;
+      window.brol.rooms = { main : [] }
+    },
+    login : function(){
+      window.credential = this.credential;
+      window.brol.credential = credential;
+      window.socket = io();
+      window.socket.emit('authentication', {username : this.credential.username, password : this.credential.password});
+      window.socket.on('authenticated', function(){
+        console.log('authenticated');
+        window.brol.authenticated = true;
+        window.auth.authenticated = true;
+      })
+      
+      // Events
+      window.socket.on('unauthorized', function(err) {
+        alert(err.message);
+        window.brol.askCredential();
+      });
+      
+      window.socket.on('authenticated', function(){
+        window.socket.on('message', function(msg) {
+          console.log(msg);
+        	if (msg.recipient) {
+            var room = msg.username;
+    	      if (window.credential.username != msg.recipient) {
+              room = msg.recipient;
+            }
+    	      window.brol.switchRoom(room);
+            return window.brol.rooms[room].messages.push(msg);
+        	}
+          window.brol.rooms.main.messages.push(msg);
+        });
+        
+        window.socket.on('onlineUsers', function(online) {
+          window.brol.online = online;
+        })
+      })
+    }
+  }
 })
-
