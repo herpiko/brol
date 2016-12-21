5c816b5bd79752b5184e9ce4fa0a53611bef22bf
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-12-21 17:14:00 +0700

    Use db url from config.

--- socket/db.js
+++ socket/db.js
@@ -2,6 +2,6 @@
 
 const mongoose = require('mongoose');
 const config = require('../config');
-mongoose.connect('mongodb://localhost/brol');
+mongoose.connect(config.mongodb);
 
 module.exports = mongoose;
